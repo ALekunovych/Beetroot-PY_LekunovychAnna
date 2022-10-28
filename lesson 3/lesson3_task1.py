516133acd5f6c6d5a03a2f33b92a8eca8d567925
ALekunovych <annalekunovych@gmail.com>
2022-10-28 23:36:25 +0300

    second commit

---
+++ lesson 3/lesson3_task1.py
@@ -0,0 +1,10 @@
+my_name = "Anna"
+day = "Friday"
+print(f"Good day {my_name}! {day} is a perfect day to learn some python.")
+
+s="Good day {}! {} is a perfect day to learn some python."
+print(s.format(my_name, day))
+
+print("Good day {0}! {1} is a perfect day to learn some python." . format (my_name, day))
+
+print("Good day %s! %s is a perfect day to learn some python." % (my_name, day))
