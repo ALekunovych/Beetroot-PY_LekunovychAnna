e06702098f55ded4bb95955a2eb2c190864daaa9
ALekunovych <annalekunovych@gmail.com>
2022-11-01 23:40:36 +0200

    updated

--- lesson 4/lesson 4_task 1.py
+++ lesson 4/lesson 4_task 1.py
@@ -1,4 +1,4 @@
-# Sample String: 'helloworld' / Expected Result : 'held'
+# Write a Python program to get a string made of the first 2 and the last 2 chars from a given string. If the string length is less than 2, return instead of the empty string.
 
 a = input("Type your message here: \n")
 if len(a) > 4 :
