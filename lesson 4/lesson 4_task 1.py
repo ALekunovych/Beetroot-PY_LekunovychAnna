48fa13ee79b0bd904e6ca8ddeb1a773c7d2aecaf
ALekunovych <annalekunovych@gmail.com>
2022-11-01 23:39:02 +0200

    updated

--- lesson 4/lesson 4_task 1.py
+++ lesson 4/lesson 4_task 1.py
@@ -1,26 +1,11 @@
 # Sample String: 'helloworld' / Expected Result : 'held'
 
-a1 = 'helloworld'
-i = 0
-
-while i < len(a1):
-    print(a1[0] + a1[1] + a1[-2] + a1[-1])
-    i += 1
-    break
-
-#Sample String: 'my' / Expected Result : 'mymy'
-
-a2 = "my"
-i = 0
-while i < len(a2):
-    print(a2[0] + a2[1], end="")
-    i += 1
-
-
-
-# Sample String: 'x' /Expected Result: Empty String
-
-b1 = "x"
-i = 0
-if len(b1) < 2:
-    print("\nEmpty string")
+a = input("Type your message here: \n")
+if len(a) > 4 :
+    a = a[0:2] + a[-2:]
+    print(a)
+elif len(a) == 2:
+    a = a*2
+    print(a)
+elif len(a) < 2:
+    print("")
